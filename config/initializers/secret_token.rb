1ffc34a6ccd38af0e068322671baaba12cdaa249
Zhan Honglai <502089948@163.com>
2014-12-09 21:34:19 +0800

    user and micropost

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ToyApp::Application.config.secret_key_base = 'e4cf750d7fd852685194b41724f63b137944805bf2b8e9e89c85c4fa13964d4aca4e30b5983665d1f79f2adb922a0c5efcac1e6d9fea3bb19434266312fb5001'
