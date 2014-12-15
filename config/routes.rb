00d0e1fd1d23ea02e445a2ff9f495ddd13d64871
honglai <gongmingqm10@foxmail.com>
2014-12-15 16:18:14 +0800

    change home page to weicha

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ ToyApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'microposts#index'
+  root 'work#weicha'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
